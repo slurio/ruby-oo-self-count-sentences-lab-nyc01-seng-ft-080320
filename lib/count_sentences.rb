fb2b547c1dd147b66e05bffbaa25c37cdb0ec038
slurio <samantha.lurio@gmail.com>
2020-08-12 18:59:26 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,21 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count_array = self.split(/\.|\?|\!/)#.count.delete(" ")
+    count_array.delete("")
+    count_array.count
+    #binding.pry
   end
 end
